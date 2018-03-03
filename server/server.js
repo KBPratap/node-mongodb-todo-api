2a52bae594d78a69597e019fd8ed087314726e8e
bhanuka <bhanu.karumuri@citrix.com>
2018-03-03 14:29:51 -0800

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     console.log(req.body);
-    var todo = new Todo({
+    let todo = new Todo({
         text: req.body.text
     })
     todo.save().then(
@@ -22,8 +22,18 @@ app.post('/todos', (req, res) => {
             res.status(400).send(e) 
         }
     )
-
 });
+
+app.get('/todos', (req, res) => {
+    Todo.find().then(
+        (todos) => {
+            res.send({todos});
+        }, 
+        (e) => {
+            res.status(400).send(e);
+        }
+    )
+})
 
 app.listen(3000, () => {
     console.log('Started on port 3000');

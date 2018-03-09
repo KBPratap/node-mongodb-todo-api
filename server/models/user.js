1a1004e042f1b0a1a929d56ee177c3972b81963a
kbpratap <kbpratap@gmail.com>
2018-03-09 01:06:41 -0800

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -79,6 +79,39 @@ UserSchema.statics.findByToken = function(token) {
     })
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    let User = this;
+    
+    return User.findOne({email})
+        .then(user => {
+            if(!user) {
+                return Promise.reject();
+            }
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, user.password, (err, result) => {
+                    if(result) { 
+                        resolve(user);
+                    } else {
+                        reject();
+                    }
+                })
+            })
+        })
+
+    try {
+        decoded = jwt.verify(token, salt)
+    } catch(e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    })
+}
+
+
 UserSchema.pre('save', function(next) {
     let user = this;
 
